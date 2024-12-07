0b90000d3fc0ef93da4c731c26e2341c7cd00d24
jef-mrs <jfbarbato.dev@gmail.com>
2024-12-07 14:44:04 +0100

    initial project version

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Electronic</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-title">Agenda Electronik</div>
+        <div class="navbar-menu"><span class="material-symbols-outlined">person_add</span></div>
+    </div>
+</body>
+</html>
