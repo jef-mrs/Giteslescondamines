65eb7cf050348e4f42066613f79990b8eb57d5c1
jef-mrs <jfbarbato.dev@gmail.com>
2024-12-08 10:37:11 +0100

    refactorisation des routes + restyling css

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
     require 'src/database/connect.php';
     $statement = $database->prepare('SELECT * FROM contact ORDER BY nom ASC');
-    $isContact = $statement->execute();
-
-    if($isContact){
+    if($statement->execute()){
         $contacts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $message = "Recuperation des contacts réussi";
+    }else{
+        $message = "Erreur lors de la récupération des contacts.";
+        exit;  // Stop le script si une erreur survient. ici, on ne fait rien de plus.
     }
+
 ?>
 
 
@@ -20,20 +23,23 @@
 
 </head>
 <body>
-
+    <p class="notification"><?php echo $message; ?></p>
     <?php include 'src/view/template/navbar.php'; ?>
     <h1 id="list-title">Liste des contacts</h1>
-    <div class="list">
-        <?php foreach($contacts as $contact):?>
-            <div class="list-item">
-                <h4><span class="material-symbols-outlined">person</span><?php echo $contact['prenom']. " ".$contact['nom'] ?></h4>
-                <p><span class="material-symbols-outlined">mail</span><?php echo $contact['email']?></p>
-                <p><span class="material-symbols-outlined">call</span><?php echo $contact['telephone']?></p>
-                <p><span class="material-symbols-outlined">source_environment</span><?php echo $contact['entreprise']?></p>
-            </div>
-        <?php endforeach;?>
-    </div>
-    </div>
+    <?php if(isset($contacts) AND !empty($contacts)): ?>
+        <div class="list">
+            <?php foreach($contacts as $contact):?>
+                <div class="list-item">
+                    <h4><span class="material-symbols-outlined">person</span><?php echo $contact['prenom']. " ".$contact['nom'] ?></h4>
+                    <p><span class="material-symbols-outlined">mail</span><?php echo $contact['email']?></p>
+                    <p><span class="material-symbols-outlined">call</span><?php echo $contact['telephone']?></p>
+                    <p><span class="material-symbols-outlined">source_environment</span><?php echo $contact['entreprise']?></p>
+                </div>
+            <?php endforeach;?>
+        </div>
+    <?php else: ?>
+        <p>Aucun contact trouvé.</p>
+    <?php endif;?>
 
 </body>
 </html>
--- src/view/contact/create.php
+++ src/view/contact/create.php
@@ -1,3 +1,22 @@
+<?php 
+
+    if(isset($_POST) AND !empty($_POST) ){
+        require_once '../../database/connect.php';
+
+
+        $statement = $database->prepare('INSERT INTO contact VALUES (NULL, :nom, :prenom, :email, :telephone, :entreprise)');
+        $statement->bindValue(':nom', $_POST["nom"], PDO::PARAM_STR);
+        $statement->bindValue(':prenom', $_POST["prenom"], PDO::PARAM_STR);
+        $statement->bindValue(':email', $_POST["email"], PDO::PARAM_STR);
+        $statement->bindValue(':telephone', $_POST["telephone"], PDO::PARAM_STR);
+        $statement->bindValue(':entreprise', $_POST["entreprise"], PDO::PARAM_STR);
+    
+        $isOK = $statement->execute();
+        $response = $isOK? "Le contact a été créé avec succès" : "Une erreur s'est produite lors de la création du contact";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +26,15 @@
     <link rel="stylesheet" href="../../../css/styles.css">
 </head>
 <body>
+    <?php if(!empty($response)): ?>
+        <p class="notification"><?php echo $response; ?></p>   
+    <?php endif; ?>
 
     <?php include '../template/navbar.php'; ?>
 
     <h1 id="form-title">Creer un contact</h1>
 
-    <form action="../../controler/contact/insert.php" method="post" class="form">
+    <form action="/electronikAgenda/src/view/contact/create.php" method="post" class="form">
         <div class="form-input">
             <input type="text" id="nom" name="nom" placeholder="Nom" required>
         </div>
@@ -20,17 +42,18 @@
             <input type="text" id="prenom" name="prenom" placeholder="Prénom" required>
         </div>
         <div class="form-input">
-            <input type="email" id="email" name="email" placeholder="Email" required>
+            <input class="material-symbols-outlined" type="email" id="email" name="email" placeholder="mail" required>
         </div>
         <div class="form-input">
-            <input type="tel" id="telephone" name="telephone" placeholder="Telephone" required>
+            <input class="material-symbols-outlined" type="tel" id="telephone" name="telephone" placeholder="call" required>
         </div>
         <div class="form-input">
-            <input type="text" id="entreprise" name="entreprise" placeholder="Entreprise" required>
+            <input class="material-symbols-outlined" type="text" id="entreprise" name="entreprise" placeholder="source_environment" required>
         </div>
-        <input class="btn" type="submit" value="Enregistrer">
-        <a class="btn" href="/electronikAgenda/index.php">Retour à la liste des contacts</a>
-
+        <div  class='btns'>
+            <input class="btn" type="submit" value="Enregistrer">     
+            <a class="btn" href="/electronikAgenda/index.php">Retour à la liste des contacts</a>
+        </div>
     </form>
 </body>
 </html>
