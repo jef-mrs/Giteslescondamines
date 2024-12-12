80789bbfd2615892406e46167a4234ada32846b7
jef-mrs <jfbarbato.dev@gmail.com>
2024-12-12 22:29:32 +0100

    create flat manager

--- lib/Manager/AbstractManager.php
+++ lib/Manager/AbstractManager.php
@@ -38,7 +38,77 @@
 
         protected function readOne(string $class, array $filters): mixed {
             $query = 'SELECT * FROM '.$this->getTableName($class).'WHERE';
+            foreach(array_keys($filters) as $filter){
+                $query .= $filter." = :".$filter;
+                if($filter != array_key_last($filters)) $query .= ' AND';
+            }
+            $stmt = $this->executeQuery($query, $filters);
+            $stmt->setFetchMode(\PDO::FETCH_CLASS, $class);
+            return $stmt->fetch();
         }
+
+        protected function readMany(string $class, array $filters = [], array $order = [], int $limit = null, int $offset = null): mixed {
+            $query = ' SELECT * FROM ' . $this->getTableName($class);
+
+            if (!empty($filters)) {
+                $query .= ' WHERE ';
+                foreach (array_keys($filters) as $filter) {
+                    $query.= $filter.' = :'.$filter;
+                    if ($filter!= array_key_last($filters)) $query.='AND ';
+                }
+            }
+
+            if (!empty($order)) {
+                $query.='ORDER BY ';
+                foreach ($order as $field => $direction) {
+                    $query.= $field.' '.$direction;
+                    if ($field!= array_key_last($order)) $query.=', ';
+                }
+            }
+
+            if ($limit!== null) {
+                $query.='LIMIT '.$limit;
+                if ($offset!== null) {
+                    $query.='OFFSET '.$offset;
+                }
+            }
+
+            $stmt = $this->executeQuery($query, $filters);
+            $stmt->setFetchMode(\PDO::FETCH_CLASS, $class);
+            return $stmt->fetchAll();
+        }
+
+        protected function create(string $class, array $fields): \PDOStatement {
+            $query = "INSERT INTO " . $this->getTableName($class) . " (";
+            foreach (array_keys($fields) as $field) {
+                $query .= $field;
+                if ($field != array_key_last($fields)) $query .= ', ';
+            }
+            $query .= ') VALUES (';
+            foreach (array_keys($fields) as $field) {
+                $query .= ':' . $field;
+                if ($field != array_key_last($fields)) $query .= ', ';
+            }
+            $query .= ')';
+            return $this->executeQuery($query, $fields);
+        }
+
+        protected function update(string $class, array $fields, int $id): \PDOStatement {
+            $query = "UPDATE " . $this->getTableName($class) . " SET ";
+            foreach (array_keys($fields) as $field) {
+                $query .= $field . " = :" . $field;
+                if ($field != array_key_last($fields)) $query .= ', ';
+            }
+            $query .= ' WHERE id = :id';
+            $fields['id'] = $id;
+            return $this->executeQuery($query, $fields);
+        }
+
+        protected function remove(string $class, int $id): \PDOStatement {
+            $query = "DELETE FROM " . $this->getTableName($class) . " WHERE id = :id";
+            return $this->executeQuery($query, [ 'id' => $id ]);
+        }
+    
     }
 
 
